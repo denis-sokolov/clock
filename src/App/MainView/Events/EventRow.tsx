9d2cfefdc0139acddf868132bfcff1fadcf7193d
Denis Sokolov <denis@sokolov.cc>
2020-09-09 12:00:00 +0300

    Event: improve time display on close events

--- src/App/MainView/Events/EventRow.tsx
+++ src/App/MainView/Events/EventRow.tsx
@@ -13,8 +13,10 @@ const clamp = (min: number, max: number, num: number) =>
 
 export function EventRow(props: Props) {
   const { event, now } = props;
-  const distanceInMs =
+  const startDistance =
     event.start === "none" ? Infinity : Math.abs(now - event.start);
+  const endDistance =
+    event.end === "none" ? Infinity : Math.abs(now - event.end);
 
   const c = makeColor(event.color);
   const color = c
@@ -26,15 +28,36 @@ export function EventRow(props: Props) {
   const minSize = 16;
   const fontSize =
     minSize +
-    (maxSize - minSize) * (1 - clamp(0, 1, distanceInMs / soonDuration));
+    (maxSize - minSize) * (1 - clamp(0, 1, startDistance / soonDuration));
   const timeFontSize = clamp(14, maxSize, fontSize / 2);
+
+  const upcoming = event.start !== "none" && now < event.start;
+  const current =
+    event.start !== "none" &&
+    event.start < now &&
+    event.end !== "none" &&
+    now < event.end;
+
+  const nMoreMins =
+    current && endDistance <= soonDuration
+      ? Math.ceil(endDistance / ms("1m"))
+      : undefined;
+  const inNmins =
+    upcoming && startDistance <= soonDuration
+      ? Math.ceil(startDistance / ms("1m"))
+      : undefined;
 
   return (
     <div className="event" style={{ color, fontSize }}>
       <span className="title">{event.title}</span>
-      {distanceInMs <= soonDuration && (
+      {nMoreMins && (
         <span className="time" style={{ fontSize: timeFontSize }}>
-          {Math.ceil(distanceInMs / ms("1m")) + "m"}
+          {nMoreMins} more {nMoreMins === 1 ? "minute" : "minutes"}
+        </span>
+      )}
+      {inNmins && (
+        <span className="time" style={{ fontSize: timeFontSize }}>
+          {inNmins < 5 && "in"} {inNmins}m
         </span>
       )}
     </div>
