e0c14c4f9f9fbf67eb130746267fe247b26cf08e
Denis Sokolov <denis@sokolov.cc>
2020-09-09 12:00:00 +0300

    Events: fix events overlap position

--- src/App/MainView/Events/index.tsx
+++ src/App/MainView/Events/index.tsx
@@ -25,20 +25,26 @@ export function Events(props: Props) {
     );
 
   const start = spacing.start || events[0]?.start;
+  const max = (ns: number[]) => Math.max.apply(Math, ns);
 
   return (
     <div>
       {events.map(function (event, i) {
-        const prevEnd = i === 0 ? start : events[i - 1].end;
+        const prevEnd = max(
+          events
+            .slice(0, i)
+            .map((e) => (e.end === "none" ? 0 : e.end))
+            .concat([start === "none" ? 0 : start])
+        );
         const ourStart = event.start;
         if (ourStart === "none")
           throw new Error(
             "Did not expect to render an event with not start date"
           );
-        const pauseSinceStart =
-          prevEnd === "none"
-            ? 0
-            : Math.min(ms("5h"), Math.max(0, ourStart - prevEnd));
+        const pauseSinceStart = Math.min(
+          ms("5h"),
+          Math.max(0, ourStart - prevEnd)
+        );
         return (
           <div
             key={event.start + event.title}
