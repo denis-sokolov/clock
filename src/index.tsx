9d4aeaa6abb43815aee8b17e6f7b52dc448b17f2
Denis Sokolov <denis@sokolov.cc>
2020-09-10 12:00:00 +0300

    main: smart outline

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { App } from "./App";
 import ReactDOM from "react-dom";
+import { smartOutline } from "@theorem/react";
 
+smartOutline();
 ReactDOM.render(<App />, document.querySelector(".app"));
