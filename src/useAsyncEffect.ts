e97e143bc9828ce58ac8f4d18e8548f25b615ab4
Denis Sokolov <denis@sokolov.cc>
2020-09-18 12:00:00 +0300

    useAsyncEffect: remove valiant efforts to provide cancellation function

--- src/useAsyncEffect.ts
+++ src/useAsyncEffect.ts
@@ -2,23 +2,15 @@ import { useEffect } from "react";
 import { useCrash } from "@theorem/react";
 
 export function useAsyncEffect(
-  f: (params: { cancelled: () => boolean }) => Promise<void | (() => void)>,
+  f: (params: { cancelled: () => boolean }) => Promise<void>,
   deps: any[]
 ) {
   const crash = useCrash();
   useEffect(function () {
     let cancelled = false;
-    let queuedCancellation = () => {};
-    f({ cancelled: () => cancelled })
-      .then((res) => {
-        if (!res) return;
-        if (cancelled) queuedCancellation();
-        else queuedCancellation = res;
-      })
-      .catch(crash);
+    f({ cancelled: () => cancelled }).catch(crash);
     return () => {
       cancelled = true;
-      queuedCancellation();
     };
   }, deps);
 }
